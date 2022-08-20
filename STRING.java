cabc7a2858c052e87d0fdf4e0b8aeaef70b608f0
jadi1422 <109712722+jadi1422@users.noreply.github.com>
2022-08-20 21:04:10 +0530

    Add files via upload

---
+++ STRING.java
@@ -0,0 +1,10 @@
+class STRING
+  {
+    public static void main(String args[])
+    {
+      String a="JADI";
+      String b=new String("LOVE JAVA");
+      System.out.println("THE GIVEN NAME IS :"+a);
+      System.out.println("THE GIVEN String IS :"+b);
+    }
+  }
